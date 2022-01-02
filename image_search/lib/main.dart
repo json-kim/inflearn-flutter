3cd89962f7cfc139dd28a7b6e8abfa44125f56cd
json-kim <flutter_dart@kakao.com>
2022-01-02 19:05:13 +0900

    feat : 의존성 주입 추가

--- image_search/lib/main.dart
+++ image_search/lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:image_search/data/data_source/pixabay_api.dart';
-import 'package:image_search/domain/use_case/get_photos_use_case.dart';
+import 'package:image_search/di/provider_setup.dart';
 import 'package:image_search/presentation/home/home_screen.dart';
-import 'package:image_search/presentation/home/home_view_model.dart';
 import 'package:provider/provider.dart';
 
-import 'data/repository/photo_api_repository_impl.dart';
-
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: globalProviders,
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -22,18 +22,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ChangeNotifierProvider(
-        create: (_) => HomeViewModel(
-          getPhotosUseCase: GetPhotosUseCase(
-            repository: PhotoApiRepositoryImpl(
-              PixabayApi(
-                http.Client(),
-              ),
-            ),
-          ),
-        ),
-        child: const HomeScreen(),
-      ),
+      home: const HomeScreen(),
     );
   }
 }
